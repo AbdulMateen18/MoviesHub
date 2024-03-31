8112ebc3ce2540393ea0f562b989452b229fe4e1
Abdul Mateen <121253565+AbdulMateen18@users.noreply.github.com>
2024-03-31 19:12:53 +0500

    .2nd commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./App.css";
 
 //62b5750
 
-const API_URL = "http://www.omdbapi.com/?apikey=62b5750";
+const API_URL = "https://www.omdbapi.com/?apikey=62b5750";
 
 const App = () => {
   const [movies, setMovies] = useState([]);
