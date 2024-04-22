645e065beca61aa4d4982a55f3e84a29b94c1668
Abdul Mateen <121253565+AbdulMateen18@users.noreply.github.com>
2024-04-22 21:42:37 +0500

    environment variables

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./App.css";
 
 //62b5750
 
-const API_URL = `https://www.omdbapi.com/?apikey=${process.env.62b5750}`;
+const API_URL = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}`;
 
 const App = () => {
   const [movies, setMovies] = useState([]);
